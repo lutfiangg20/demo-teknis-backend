e506bbec4f44b2c827d89b12a1b690ca763242a8
Lutfi Angga <lutfiangg20@gmail.com>
2025-08-26 23:14:27 +0700

    .

--- src/main/java/lutfiangg20/demo_teknis/config/SecurityConfig.java
+++ src/main/java/lutfiangg20/demo_teknis/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
         .formLogin(form -> form.disable())
         .httpBasic(basic -> basic.disable())
         .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
             .requestMatchers("/api/users/**").authenticated()
             .requestMatchers("/api/auth/check").authenticated()
             .anyRequest().permitAll())
